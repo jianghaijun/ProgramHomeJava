433584685011e19d827b4026ebb769da33dba840
jianghaijun <aini1320666709@foxmail.com>
2017-11-02 18:44:55 +0800

    添加请求提示字符串

--- src/main/java/pres/haijun/programhome/utils/ConstantUtil.java
+++ src/main/java/pres/haijun/programhome/utils/ConstantUtil.java
@@ -29,12 +29,22 @@ public class ConstantUtil {
 	/**失败*/
 	public static final int CODE_FLAG_ONE = 1;
 	/**用户名、密码不能为空*/
-	public static final String USERNAME_PASSWORD_CAN_NOT_EMPTY = "用户名、密码不能为空!";
+	public static final String USERPHONE_PASSWORD_CAN_NOT_EMPTY = "手机号、密码不能为空!";
 	/**注册成功*/
 	public static final String REGION_SUCCESSFUL = "注册成功!";
 	/**注册失败*/
 	public static final String REGION_ERROR = "注册失败!";
 	/**该用户已经注册*/
 	public static final String USER_OLREADY_REGION = "该用户已经注册!";
+	/**登录成功*/
+	public static final String LOGIN_SUCCESSFUL = "登陆成功!";
+	/**登录失败*/
+	public static final String USERPHONE_PASSWORD_ERROR = "用户名或密码不正确!";
+	/**参数不能为空*/
+	public static final String PARAMETER_CAN_NOT_EMPTY = "参数不能为空!";
+	/**修改成功*/
+	public static final String UPDATE_SUCCESSFUL = "修改成功!";
+	/**修改失败*/
+	public static final String UPDATE_ERROR = "修改失败!";
 
 }
