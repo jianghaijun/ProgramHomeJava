274c73b87698192bf255d3aaf4c8ee8db46e7960
jianghaijun <aini1320666709@foxmail.com>
2017-11-02 18:44:27 +0800

    基础Model

--- src/main/java/pres/haijun/programhome/model/BaseModel.java
+++ src/main/java/pres/haijun/programhome/model/BaseModel.java
@@ -24,35 +24,9 @@ package pres.haijun.programhome.model;
  *  Created by HaiJun on 2017年10月30日 下午10:37:56
  */
 
-import pres.haijun.programhome.bean.BaseBean;
-/**
- * 
- *                     _ooOoo_
- *                    o8888888o
- *                    88" . "88
- *                    (| -_- |)
- *                    O\  =  /O
- *                 ____/`---'\____
- *               .'  \\|     |//  `.
- *              /  \\|||  :  |||//  \
- *             /  _||||| -:- |||||-  \
- *             |   | \\\  -  /// |   |
- *             | \_|  ''\---/''  |   |
- *             \  .-\__  `-`  ___/-. /
- *           ___`. .'  /--.--\  `. . __
- *        ."" '<  `.___\_<|>_/___.'  >'"".
- *       | | :  `- \`.;`\ _ /`;.`/ - ` : | |
- *       \  \ `-.   \_ __\ /__ _/   .-` /  /
- * ======`-.____`-.___\_____/___.-`____.-'======
- *                     `=---='
- * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
- * 			                     佛祖保佑       永无BUG
- *  Created by HaiJun on 2017年11月1日 下午9:22:22
- */
 public class BaseModel {
 	private int code;
 	private String message;
-	private BaseBean body;
 
 	public int getCode() {
 		return code;
@@ -68,14 +42,6 @@ public class BaseModel {
 
 	public void setMessage(String message) {
 		this.message = message;
-	}
-
-	public BaseBean getBody() {
-		return body;
-	}
-
-	public void setBody(BaseBean body) {
-		this.body = body;
 	}
 
 }
